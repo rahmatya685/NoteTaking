c5f454dcaf69355bde59165be02267c11d598268
Alireza Rahmati <zanjan2010@yahoo.com>
2021-03-28 18:25:42 +0430

    commenting an uncompleted unit test

--- app/src/test/java/com/example/notetaking/add_note/NoteAddEditFragmentTest.kt
+++ app/src/test/java/com/example/notetaking/add_note/NoteAddEditFragmentTest.kt
@@ -1,50 +1,50 @@
-package com.example.notetaking.add_note
-
-import android.os.Build
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.MediumTest
-import com.example.notetaking.R
-import com.example.notetaking.ServiceLocator
-import com.example.notetaking.mapper.NoteEntityToNoteMapper
-import com.example.notetaking.model.Note
-import com.example.notetaking.repo.DefaultNoteRepo
-import com.example.notetaking.repo.NoteRepo
-import com.example.notetaking.repo.local.DefaultNoteLocalDataSource
-import com.example.notetaking.repo.saveNoteBlocking
-import com.example.repo.local.FakeNoteLocalDataSource
-import junit.framework.TestCase
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
-import org.robolectric.annotation.LooperMode
-import org.robolectric.annotation.TextLayoutMode
-
-/**
- * Integration task for [NoteAddEditFragment] fragment
- */
-@MediumTest
-@RunWith(AndroidJUnit4::class)
-@ExperimentalCoroutinesApi
-@LooperMode(LooperMode.Mode.PAUSED)
-@TextLayoutMode(TextLayoutMode.Mode.REALISTIC)
-@Config(sdk = [Build.VERSION_CODES.JELLY_BEAN_MR2])
-class NoteAddEditFragmentTest {
-
-    lateinit var noteRepo: NoteRepo
-
-    @Before
-    fun setup() {
-        noteRepo = DefaultNoteRepo(FakeNoteLocalDataSource(), NoteEntityToNoteMapper())
-        ServiceLocator.noteRepo = noteRepo
-    }
-
+//package com.example.notetaking.add_note
+//
+//import android.os.Build
+//import androidx.fragment.app.testing.launchFragmentInContainer
+//import androidx.test.espresso.Espresso.onView
+//import androidx.test.espresso.assertion.ViewAssertions
+//import androidx.test.espresso.assertion.ViewAssertions.matches
+//import androidx.test.espresso.matcher.ViewMatchers.*
+//import androidx.test.ext.junit.runners.AndroidJUnit4
+//import androidx.test.filters.MediumTest
+//import com.example.notetaking.R
+//import com.example.notetaking.ServiceLocator
+//import com.example.notetaking.mapper.NoteEntityToNoteMapper
+//import com.example.notetaking.model.Note
+//import com.example.notetaking.repo.DefaultNoteRepo
+//import com.example.notetaking.repo.NoteRepo
+//import com.example.notetaking.repo.local.DefaultNoteLocalDataSource
+//import com.example.notetaking.repo.saveNoteBlocking
+//import com.example.repo.local.FakeNoteLocalDataSource
+//import junit.framework.TestCase
+//import kotlinx.coroutines.ExperimentalCoroutinesApi
+//import org.junit.Before
+//import org.junit.Test
+//import org.junit.runner.RunWith
+//import org.robolectric.annotation.Config
+//import org.robolectric.annotation.LooperMode
+//import org.robolectric.annotation.TextLayoutMode
+//
+///**
+// * Integration task for [NoteAddEditFragment] fragment
+// */
+//@MediumTest
+//@RunWith(AndroidJUnit4::class)
+//@ExperimentalCoroutinesApi
+//@LooperMode(LooperMode.Mode.PAUSED)
+//@TextLayoutMode(TextLayoutMode.Mode.REALISTIC)
+//@Config(sdk = [Build.VERSION_CODES.JELLY_BEAN_MR2])
+//class NoteAddEditFragmentTest {
+//
+//    lateinit var noteRepo: NoteRepo
+//
+//    @Before
+//    fun setup() {
+//        noteRepo = DefaultNoteRepo(FakeNoteLocalDataSource(), NoteEntityToNoteMapper())
+//        ServiceLocator.noteRepo = noteRepo
+//    }
+//
 //    @Test
 //    fun noteDetailForUpdate_showInUi() {
 //        //GIVEN - save a note on db
@@ -55,6 +55,6 @@ class NoteAddEditFragmentTest {
 //        onView(withId(R.id.tv_title)).check(matches(isDisplayed()))
 //        onView(withId(R.id.tv_content)).check(matches(isDisplayed()))
 //    }
-
-
-}
+//
+//
+//}
