1654398f1745538e72c14dd4beedf3363c36a5c6
Alireza Rahmati <zanjan2010@yahoo.com>
2021-06-02 18:02:38 +0430

    testing ci

--- app/src/main/java/com/example/notetaking/note_list/NoteListFragment.kt
+++ app/src/main/java/com/example/notetaking/note_list/NoteListFragment.kt
@@ -1,16 +1,13 @@
 package com.example.notetaking.note_list
-
-import androidx.lifecycle.ViewModelProvider
+ 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.fragment.app.viewModels 
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.notetaking.R
 import com.example.notetaking.databinding.NoteListFragmentBinding
 import com.example.notetaking.getViewModelsFactory
@@ -23,7 +20,7 @@ class NoteListFragment : Fragment() {
 
     private val binding get() = _binding!!
 
-    private val adator = NoteListAdaptor()
+    private val adaptor = NoteListAdaptor()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,7 +33,7 @@ class NoteListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
         btnAdd.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.addNoteFragment))
-        rvNotes.adapter = adator
+        rvNotes.adapter = adaptor
         rvNotes.layoutManager = LinearLayoutManager(requireContext())
         viewModel.notes.observe(viewLifecycleOwner, {
             it?.let { notes ->
@@ -49,7 +46,7 @@ class NoteListFragment : Fragment() {
                         noDataLayout.visibility = View.VISIBLE
                     }
                 }
-                adator.submitList(notes)
+                adaptor.submitList(notes)
             }
         })
         viewModel.loadNotes()
